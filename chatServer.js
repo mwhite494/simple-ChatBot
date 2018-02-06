202e93af81691f6bee5d70fd3c4b43043f1e2d60
mwhite494 <mwhite494@gmail.com>
2018-02-05 19:43:02 -0500

    Create a GoT trivia bot

--- chatServer.js
+++ chatServer.js
@@ -30,8 +30,8 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-  setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
+  socket.emit('answer',"Hello I am Jon Snow, a sworn brother of the Nights Watch."); //We start with the introduction;
+  setTimeout(timedQuestion, 2500, socket,"What is your name?"); // Wait a moment and respond with a question.
 
 });
   socket.on('message', (data)=>{ // If we get a new message from the client we process it;
@@ -51,50 +51,85 @@ function bot(data,socket,questionNum) {
 
 /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
-  waitTime =2000;
-  question = 'How old are you?';			    	// load next question
+  answer = 'Hello ' + input + '. Winter is coming.';// output response
+  waitTime = 2500;
+  question = 'I will test your knowledge of Westeros. What is my former house?';			    	// load next question
   }
   else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
+    waitTime = 2500;
+    if(input.toLowerCase().includes('stark')){
+      answer = 'Tis true, it\'s a shame what happened to my family.';
+    }
+    else {
+      answer = 'Fool! Who are you?! A Lannister! I owe my allegiance to House Stark!'
+    }
+    question = 'What is the name of my direwolf?';			    	// load next question
   }
   else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
-  waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
+    waitTime = 2500;
+    if(input.toLowerCase().includes('ghost')){
+      answer = 'Ghost, my good old friend. Raised him since he was a pup.';
+    }
+    else {
+      answer = 'I should have your head on a spike! My wolf\'s name is Ghost!';
+    }
+    question = 'What is the real name belonging to \'The Mountain\'?';			    	// load next question
   }
   else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
-  socket.emit('changeBG',input.toLowerCase());
-  waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
+    waitTime = 2500;
+    if(input.toLowerCase().includes('gregor clegane')){
+      answer = 'Yes, curse him. He is a threat as long as he is by Cersei\'s side.';
+    }
+    else {
+      answer = 'Fool! His name is Gregor Clegane!';
+    }
+    question = 'Do you know the nickname given to his brother Sandor Clegane?';
   }
   else if (questionNum == 4) {
-    if(input.toLowerCase()==='yes'|| input===1){
-      answer = 'Perfect!';
-      waitTime =2000;
-      question = 'Whats your favorite place?';
+    waitTime = 2500;
+    if(input.toLowerCase().includes('hound')){
+      answer = 'Tis true, I fought with him beyond The Wall.';
     }
-    else if(input.toLowerCase()==='no'|| input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
-        answer='How about now?'
-        question='';
-        waitTime =0;
-        questionNum--; // Here we go back in the question number this can end up in a loop
-    }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
-      question='';
-      questionNum--;
-      waitTime =0;
+    else{
+      answer = 'You bring dishonor to my house! His name was The Hound.';
     }
-  // load next question
+    question = 'Do you know the name of my mother?';
   }
-  else{
-    answer= 'I have nothing more to say!';// output response
-    waitTime =0;
-    question = '';
+  else if (questionNum == 5) {
+    waitTime = 2500;
+    if(input.toLowerCase().includes('lyanna')){
+      answer = 'Wait... what? I never knew...';
+    }
+    else{
+      answer = 'No that can not be! I\'m still waiting for someone to tell me...';
+    }
+    question = 'What is the name of my sword?';
+  }
+  else if (questionNum == 6) {
+    waitTime = 2500;
+    if(input.toLowerCase().includes('longclaw') || input.toLowerCase().includes('long claw')){
+      answer = 'Yes! A great sword made from Valyrian steel.';
+    }
+    else{
+      answer = 'Fool! The name of my sword is Longclaw.';
+    }
+    question = 'Will you aid me in the Great War to come?';
+  }
+  else {
+    if(input.toLowerCase()==='yes' || input===1){
+      answer = 'Good. We need ever soldier we can get. Goodbye!';
+      waitTime = 0;
+      question = '';
+    }
+    else if(input.toLowerCase()==='no' || input===0){
+        answer = 'Well then you best ride south. Goodbye!'
+        waitTime = 0;
+        question = '';
+    }else{
+      answer='I did not understand you. Can you please answer with a yes or no.'
+      question='Will you aid me in the Great War to come?';
+      waitTime = 2000;
+    }
   }
 
 
@@ -107,7 +142,7 @@ function bot(data,socket,questionNum) {
 function timedQuestion(socket,question) {
   if(question!=''){
   socket.emit('question',question);
-}
+  }
   else{
     //console.log('No Question send!');
   }
